ed9f412f5d6579032e69468047f2e3e8342824bd
JarrenJ <j.jarren@yahoo.com>
2021-03-30 10:35:05 -0700

    Switched cache method to use disk storage instead of RAM. Currently stores in a default Caches directory on my system

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ if __name__ == "__main__":
     # print(f'Cache: {cache.API_CACHE}')
     # print(f'Default Cache: {cache.get_default_cache()}')
     # client = pokepy.V2Client()
-    client_mem_cache = pokepy.V2Client(cache='in_memory')
-    print(f'Default Cache: {client_mem_cache.get_pokemon.cache_info()}')
+    # client_mem_cache = pokepy.V2Client(cache='in_memory')
+    client_disk_cache = pokepy.V2Client(cache='in_disk')
+    print(f'Disk Cache: {client_disk_cache.get_pokemon.cache_location()}')
+    print(f'Default Cache: {client_disk_cache.get_pokemon.cache_info()}')
     gen1_range = 152
     gen1_ids = [i for i in range(1, gen1_range)]
-    gen1_names = [client_mem_cache.get_pokemon(i) for i in range(1, gen1_range)]
+    gen1_names = [client_disk_cache.get_pokemon(i) for i in range(1, gen1_range)]
     zipped = zip(gen1_ids, gen1_names)
     gen1 = dict(zipped)
     for poke in gen1:
         print(f'Poke: {gen1.get(poke).name}')
         for type_slot in gen1.get(poke).types:
             print('{} - {}: {}'.format(gen1.get(poke).name, type_slot.slot, type_slot.type.name.title()))
-    print(f'Cache Info: {client_mem_cache.get_pokemon.cache_info()}')
+    print(f'Cache Info: {client_disk_cache.get_pokemon.cache_info()}')
 
     # CACHED
     print('BEGIN CACHED')
     # gen1_range = 152
     # gen1_ids = [i for i in range(1, gen1_range)]
-    gen1_names = [client_mem_cache.get_pokemon(i) for i in range(1, gen1_range)]
+    gen1_names = [client_disk_cache.get_pokemon(i) for i in range(1, gen1_range)]
     zipped = zip(gen1_ids, gen1_names)
     gen1 = dict(zipped)
     for poke in gen1:
@@ -32,7 +34,7 @@ if __name__ == "__main__":
         for type_slot in gen1.get(poke).types:
             print('{} - {}: {}'.format(gen1.get(poke).name, type_slot.slot, type_slot.type.name.title()))
 
-    print(f'Cache Info: {client_mem_cache.get_pokemon.cache_info()}')
+    print(f'Cache Info: {client_disk_cache.get_pokemon.cache_info()}')
 
     # client_mem_cache.get_pokemon.cache_clear()
     # print(f'Pokemon : {client_mem_cache.get_pokemon(14)}')
